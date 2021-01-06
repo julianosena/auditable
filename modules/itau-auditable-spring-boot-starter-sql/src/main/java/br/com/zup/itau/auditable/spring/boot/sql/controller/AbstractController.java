e9d949bfc05e600fdd71ca7aec480539ec774606
Juliano Sena da Silva Carlos <juliano.carlos@zup.com.br>
2021-01-06 17:14:23 -0300

    UseCase and Controller para busca de revisoes

--- modules/itau-auditable-spring-boot-starter-sql/src/main/java/br/com/zup/itau/auditable/spring/boot/sql/controller/AbstractController.java
+++ modules/itau-auditable-spring-boot-starter-sql/src/main/java/br/com/zup/itau/auditable/spring/boot/sql/controller/AbstractController.java
@@ -1,14 +1,20 @@
 package br.com.zup.itau.auditable.spring.boot.sql.controller;
 
+import br.com.zup.itau.auditable.spring.boot.sql.usecase.GetRevisionsByIdAndTypeUseCase;
+import br.com.zup.itau.auditable.usecase.exception.ItauAuditableUseCaseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 abstract public class AbstractController {
 
-    public abstract String getType();
+    @Autowired
+    private GetRevisionsByIdAndTypeUseCase getRevisionsByIdAndTypeUseCase;
 
     @GetMapping("/revisions/{id}")
-    public void execute(@PathVariable("id") int id) {
+    public void execute(@PathVariable("id") Long id) throws ItauAuditableUseCaseException {
         this.getRevisionsByIdAndTypeUseCase.execute(id, this.getType());
     }
+
+    public abstract String getType();
 }
