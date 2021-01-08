4e46455d040eb6882bdb4524a9a2a7fbb33c6922
lucaspaixaozup <lucas.paixao@zup.com.br>
2021-01-08 13:16:27 -0300

    fixed audit_commit

--- modules/itau-auditable-spring-boot-starter-sql/src/main/java/br/com/zup/itau/auditable/spring/boot/sql/gateway/database/model/CommitDatabase.java
+++ modules/itau-auditable-spring-boot-starter-sql/src/main/java/br/com/zup/itau/auditable/spring/boot/sql/gateway/database/model/CommitDatabase.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "jv_commit")
+@Table(name = "audit_commit")
 public class CommitDatabase implements Serializable {
 
     private static final long serialVersionUID = 1L;
