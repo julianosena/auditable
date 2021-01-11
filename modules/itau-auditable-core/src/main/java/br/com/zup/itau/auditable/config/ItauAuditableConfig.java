8ce46e54113b643001d5e26b849bd54dead1060c
Juliano Sena da Silva Carlos <juliano.carlos@zup.com.br>
2021-01-11 18:38:05 -0300

    Criando classe para configuracão

---
+++ modules/itau-auditable-core/src/main/java/br/com/zup/itau/auditable/config/ItauAuditableConfig.java
@@ -0,0 +1,7 @@
+package br.com.zup.itau.auditable.config;
+
+public class ItauAuditableConfig {
+
+    public static final String ITAU_AUDITABLE_FULL_QUALIFIED_PACKAGE = "br.com.zup.itau.auditable";
+
+}
--- modules/itau-auditable-spring-boot-starter-sql/src/main/java/br/com/zup/itau/auditable/spring/boot/sql/config/SqlConfiguration.java
+++ modules/itau-auditable-spring-boot-starter-sql/src/main/java/br/com/zup/itau/auditable/spring/boot/sql/config/SqlConfiguration.java
@@ -1,8 +1,8 @@
 package br.com.zup.itau.auditable.spring.boot.sql.config;
 
-public class SqlConfiguration {
+import static br.com.zup.itau.auditable.config.ItauAuditableConfig.ITAU_AUDITABLE_FULL_QUALIFIED_PACKAGE;
 
-    public static final String ITAU_AUDITABLE_FULL_QUALIFIED_PACKAGE = "br.com.zup.itau.auditable";
+public class SqlConfiguration {
 
     public static final String ITAU_AUDITABLE_ENTITY_FULL_QUALIFIED_PACKAGE = ITAU_AUDITABLE_FULL_QUALIFIED_PACKAGE + ".spring.boot.sql.gateway.database.model";
 
