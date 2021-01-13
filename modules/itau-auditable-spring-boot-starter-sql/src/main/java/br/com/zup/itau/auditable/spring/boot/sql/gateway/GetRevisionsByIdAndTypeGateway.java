69e0383f1bd952bc0ad78c918d0f3bacc5f45bf8
Juliano Sena da Silva Carlos <juliano.carlos@zup.com.br>
2021-01-13 11:53:33 -0300

    Ajustando controller para retornar revisions

--- modules/itau-auditable-spring-boot-starter-sql/src/main/java/br/com/zup/itau/auditable/spring/boot/sql/gateway/GetRevisionsByIdAndTypeGateway.java
+++ modules/itau-auditable-spring-boot-starter-sql/src/main/java/br/com/zup/itau/auditable/spring/boot/sql/gateway/GetRevisionsByIdAndTypeGateway.java
@@ -3,8 +3,6 @@ package br.com.zup.itau.auditable.spring.boot.sql.gateway;
 import br.com.zup.itau.auditable.spring.boot.sql.domain.GlobalId;
 import br.com.zup.itau.auditable.spring.boot.sql.gateway.exception.ItauAuditableGatewayException;
 
-import java.util.List;
-
 public interface GetRevisionsByIdAndTypeGateway {
-    List<GlobalId> execute(String id, String type) throws ItauAuditableGatewayException;
+    GlobalId execute(String id, String type) throws ItauAuditableGatewayException;
 }
